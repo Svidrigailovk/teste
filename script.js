67c0f7aae6921374a8893a3eec7ad28765716e14
Your Name <you@example.com>
2025-05-16 13:40:54 -0300

    Novo commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function openAlert(className) {
+    if (className == 'vermelho') {
+        alert('vermelho')
+    } else {
+        alert('verde')
+    }
+}
+
+//mudar cor da div
+let colorVariable = false
+function changeColor() {
+    colorVariable = !colorVariable
+
+    if (colorVariable == false) {
+        openAlert('vermelho')
+        document.getElementById("colorDiv").className = "red";
+    } else {
+        openAlert('verde')
+        document.getElementById("colorDiv").className = "green";
+    }
+}
